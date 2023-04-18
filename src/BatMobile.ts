793acbb3d18a244eac09da0bd2c5c95c3f753ee0
SOKI.CHHUOY <SOKI.CHHUOY@PNC.LAN>
2023-04-18 08:38:51 +0700

    add class

---
+++ src/BatMobile.ts
@@ -0,0 +1,4 @@
+export class BatMobile {
+    isBatmanHere: boolean
+
+}
---
+++ src/MiniVan.ts
@@ -0,0 +1,8 @@
+export class MiniVan{
+    numberCustomer: number
+    numberLuggage: number
+    constructor( numberCustomer: number,numberLuggage: number){
+        this.numberCustomer = numberCustomer
+        this.numberLuggage = numberLuggage
+    }
+}
